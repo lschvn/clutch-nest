c4a9fcb6d73ae0ecf1b69cdf2f2aaa0da618438b
LouisLSCHVN <louis.leschevin@gmail.com>
2025-06-25 00:46:45 +0200

    feat(elo): add constants for Elo rating calculations and match parameters

---
+++ src/core/elo/elo.constants.ts
@@ -0,0 +1,6 @@
+export const INITIAL_RATING = 1500;
+export const K_BASE = 20;
+export const RECENT_WINDOW_DAYS = 60;
+export const RECENT_MATCH_BONUS = 2;
+export const DECAY_LAMBDA = 0.0007;
+export const MAX_MARGIN_BONUS = 1.25;
