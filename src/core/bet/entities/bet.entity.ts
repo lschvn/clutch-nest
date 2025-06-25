63a6cd704f1b2b55cabfb3e672d2382696b299d8
LouisLSCHVN <louis.leschevin@gmail.com>
2025-06-25 09:36:16 +0200

    feat(bet): implement Bet entity with relationships to User and Match, and add timestamp columns

--- src/core/bet/entities/bet.entity.ts
+++ src/core/bet/entities/bet.entity.ts
@@ -1,1 +1,34 @@
-export class Bet {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Match } from '../../matches/entities/match.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity({ name: 'bets' })
+export class Bet {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Match)
+  @JoinColumn({ name: 'match_id' })
+  match: Match;
+
+  @Column({ type: 'int' })
+  tokens: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
